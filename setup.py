efb2d75ada029cc2becd3a4a42c701265925cf9f
Joel Smith <joelsmith@Joels-MacBook-Air.local>
2024-05-13 09:44:43 -0700

    install is working

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='edatools',
       version='0.1',
       description='A set of functions for data cleaning and exploratory data analysis',
       author='Joel Smith',
       author_email='jhavsmi@pm.me',
-      packages=find_packages(),
+      packages=['edatools'],
       install_requires=[
             'pandas', 'datetime'
       ]
