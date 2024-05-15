05473767a26a184b4a2a8b999b06c2865fe9299c
Joel Smith <joelsmith@Joels-MacBook-Air.local>
2024-05-15 15:01:41 -0700

    na

--- edatools/functions.py
+++ edatools/functions.py
@@ -2,7 +2,15 @@ import datetime as dt
 import polars as pl
 
 def dtvars(df_input, dtcols, dtlabels):
-    """Creates multiple time-related columns out of a datetime column using polars"""
+    '''
+    Creates multiple time-related columns out of a datetime column using polars
+    
+    Arguments:
+        df_input:
+        dtcols:
+        dtlabels:
+
+    '''
     for i in range(len(dtcols)):
         df_input = df_input.with_columns(
         pl.col(dtcols[i]).dt.year().alias(dtlabels[i]+'_year'),
@@ -16,7 +24,16 @@ def dtvars(df_input, dtcols, dtlabels):
     return df_input
 
 def cleaning_record(df, step_label, starting_rows='NA', clean_df='NA'):
-    """Records the number of removed and remaining rows for a given data cleaning step"""
+    '''
+    Records the number of removed and remaining rows for a given data cleaning step
+    
+    Arguments:
+        df:
+        step_label:
+        starting_rows:
+        clean_df:
+
+    '''
     if not isinstance(clean_df, pl.DataFrame):
         row_count = starting_rows-df.shape[0]
         clean_df = pl.DataFrame(data={'Cleaning Step': step_label,'Rows Removed':row_count,'Rows Remaining':df.shape[0]})
@@ -27,7 +44,14 @@ def cleaning_record(df, step_label, starting_rows='NA', clean_df='NA'):
     return clean_df
 
 def weekday_names(df_input, wdcols):
-    """Converts numerical weekdays into abbreviated weekday names"""
+    '''
+    Converts numerical weekdays into abbreviated weekday names
+    
+    Arguments:
+        df_input:
+        wdcols:
+        
+    '''
     dtype = pl.Enum(["Sun", "Mon", "Tue","Wed","Thu","Fri","Sat"])
     for i in range(len(wdcols)):
         df_output = df_input.with_columns(
@@ -37,9 +61,46 @@ def weekday_names(df_input, wdcols):
     return df_output
 
 def missing_data(df_input):
+    '''
+    Computes missing values and data type for each column in a polars dataframe.
+
+    Arguments:
+        df_input
+
+    '''
     df_missing = pl.DataFrame({'columns':df_input.columns,
                                'dtypes':df_input.dtypes,
                                'null_values':df_input.null_count().row(0)})
     return df_missing
 
+def outlier_imputer(column_list, iqr_factor):
+    '''
+    Impute upper-limit values in specified columns based on their interquartile range.
+
+    Arguments:
+        column_list: A list of columns to iterate over
+        iqr_factor: A number representing x in the formula:
+                    Q3 + (x * IQR). Used to determine maximum threshold,
+                    beyond which a point is considered an outlier.
+
+    The IQR is computed for each column in column_list and values exceeding
+    the upper threshold for each column are imputed with the upper threshold value.
+    '''
+    for col in column_list:
+        # Reassign minimum to zero
+        df.loc[df[col] < 0, col] = 0
+
+        # Calculate upper threshold
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        iqr = q3 - q1
+        upper_threshold = q3 + (iqr_factor * iqr)
+        print(col)
+        print('q3:', q3)
+        print('upper_threshold:', upper_threshold)
+
+        # Reassign values > threshold to threshold
+        df.loc[df[col] > upper_threshold, col] = upper_threshold
+        print(df[col].describe())
+        print()
 
